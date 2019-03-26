a0afb683dfe4f951f6310b1751b3046542b8ab77
DANIL BOGDANOV <danilbogdanov@danils-Mac-mini.local>
2019-03-26 13:45:34 +0300

    work curent values

--- monitor.py
+++ monitor.py
@@ -10,34 +10,53 @@ REQUEST_KWARGS={
     }
 }
 
-updater = Updater(token='878214304:AAGZJPvnRGHOFf2lQMnRUFUVJx-R16eXcg4', use_context=True,request_kwargs=REQUEST_KWARGS)
-jobq = updater.job_queue
+#updater = Updater(token='878214304:AAGZJPvnRGHOFf2lQMnRUFUVJx-R16eXcg4', use_context=True,request_kwargs=REQUEST_KWARGS)
+#jobq = updater.job_queue
 
 sess=requests.Session()
+def auth():
+    auth=sess.post('https://api.owencloud.ru/v1/auth/open',json={"login":"sibai3@gmail.ru","password":"111111"})
+    return auth.json()
 
-auth=sess.post('https://api.owencloud.ru/v1/auth/open',json={"login":"sibai3@gmail.ru","password":"111111"})
-r=sess.post('https://api.owencloud.ru/v1/device/index',headers={'Content-Type':'application/json', 'Authorization': 'Bearer {}'.format(auth.json()['token'])})
-devs={}
-events={}
-for devices in r.json():
-    devs[devices['id']]=devices['name']
-print(devs)
-lts = datetime.now() - timedelta(minutes=+5)
-r4 = sess.post('https://api.owencloud.ru//v1/device/events-log-backward/63934',headers={'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(auth.json()['token'])},json={"end": lts.strftime('%Y-%m-%d %H:%M:%S'), "limit": "10"})
-print(lts.strftime('%Y-%m-%d %H:%M:%S'))
-last_event = r4.json()
-print(last_event)
-ts = int(last_event[0]['start_dt'])
-print(devs[last_event[0]['device_id']], last_event[0]['message'], last_event[0]['data'][0]['v'],
-              datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+def device_list(auth):
+    r=sess.post('https://api.owencloud.ru/v1/device/index',headers={'Content-Type':'application/json', 'Authorization': 'Bearer {}'.format(auth['token'])})
+    devs={}
+    for devices in r.json():
+        devs[devices['id']]=devices['name']
+    return devs
+
+def get_params(device,auth):
+    urls='https://api.owencloud.ru/v1/device/'+str(device)
+    data = sess.post(urls,headers={'Content-Type':'application/json', 'Authorization': 'Bearer {}'.format(auth['token'])})
+    return  data.json()['parameters']
 
 
+
+def get_data(param):
+    payload = {"ids": param}
+    data=sess.post('https://api.owencloud.ru/v1/parameters/last-data',headers={'Content-Type':'application/json', 'Authorization': 'Bearer {}'.format(auth()['token'])},json=payload)
+    return data.json()
+
+def current_values():
+    for dev in device_list(auth()).keys():
+        for p in get_params(dev,auth()):
+            if p['name']=='Значение float 1':
+                print(device_list(auth())[dev],get_data(p['id']))
 
 
 def callback_minute(context):
     context.bot.send_message(chat_id='@AIR_sibay',text=str(devs[last_event[0]['device_id']])+"\n"+str(last_event[0]['message'])+"\nТекущее значение :"+str(last_event[0]['data'][0]['v'])+"\n"+str(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')))
 
-job_minute = jobq.run_repeating(callback_minute, interval=60, first=0)
+
+
+
+
+
+
+#job_minute = jobq.run_repeating(callback_minute, interval=60, first=0)
 
 #updater.start_polling()
 #updater.idle()
+
+
+current_values()
