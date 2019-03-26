d03717c8c858b7870bdd76ebc652a45302f4faee
DANIL BOGDANOV <danilbogdanov@danils-Mac-mini.local>
2019-03-26 17:07:30 +0300

    lol

--- monitor.py
+++ monitor.py
@@ -2,21 +2,23 @@ import requests
 from datetime import timedelta
 from telegram.ext import Updater
 from datetime import datetime
+
 REQUEST_KWARGS={
     'proxy_url': 'socks5://proxy.ec-host.ru:1080',
     'urllib3_proxy_kwargs': {
         'username': 'jackv',
-        'password': '00000000',
+        'password': '00000',
     }
 }
 
-#updater = Updater(token='878214304:AAGZJPvnRGHOFf2lQMnRUFUVJx-R16eXcg4', use_context=True,request_kwargs=REQUEST_KWARGS)
-#jobq = updater.job_queue
-
+updater = Updater(token='0000',request_kwargs=REQUEST_KWARGS, use_context=True)
+jobq = updater.job_queue
 sess=requests.Session()
+
+
 def auth():
-    auth=sess.post('https://api.owencloud.ru/v1/auth/open',json={"login":"sibai3@gmail.ru","password":"111111"})
-    return auth.json()
+    au=sess.post('https://api.owencloud.ru/v1/auth/open',json={"login":"sibai3@gmail.ru","password":"111111"})
+    return au.json()
 
 def device_list(auth):
     r=sess.post('https://api.owencloud.ru/v1/device/index',headers={'Content-Type':'application/json', 'Authorization': 'Bearer {}'.format(auth['token'])})
@@ -38,25 +40,29 @@ def get_data(param):
     return data.json()
 
 def current_values():
+
+    response_list = []
     for dev in device_list(auth()).keys():
+        response = {}
         for p in get_params(dev,auth()):
             if p['name']=='Значение float 1':
-                print(device_list(auth())[dev],get_data(p['id']))
-
+                response['street']=device_list(auth())[dev]
+                response['value']=get_data(p['id'])[0]['values'][0]['f']
+                response['time']=datetime.utcfromtimestamp(int(get_data(p['id'])[0]['values'][0]['d'])).strftime('%Y-%m-%d %H:%M:%S')
+        response_list.append(response)
+    return response_list
 
 def callback_minute(context):
-    context.bot.send_message(chat_id='@AIR_sibay',text=str(devs[last_event[0]['device_id']])+"\n"+str(last_event[0]['message'])+"\nТекущее значение :"+str(last_event[0]['data'][0]['v'])+"\n"+str(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')))
+    context.bot.send_message(chat_id='@AIR_sibay',text='message')
 
 
 
 
 
 
+job_minute = jobq.run_repeating(callback_minute, interval=60, first=0)
 
-#job_minute = jobq.run_repeating(callback_minute, interval=60, first=0)
-
-#updater.start_polling()
-#updater.idle()
+updater.start_polling()
+updater.idle()
 
 
-current_values()
