bdec0e4c2a86e2e3aedef483e4c7e662b3af2b78
jackv <jackv@MacBook-Pro-jackv.local>
2019-06-18 18:37:39 +0300

    add bot

--- monitor_apps/src/bot_app.py
+++ monitor_apps/src/bot_app.py
@@ -1,34 +1,27 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler,Filters
 from telegram import KeyboardButton, ReplyKeyboardMarkup
-updater = Updater('878214304:AAE9u2Lym2llC3FEsag0pu5mXeD96AQf1XA', use_context=True)
+import configparser
+
+config = configparser.ConfigParser()
+config_path=os.path.join(os.path.dirname(__file__), 'config.ini')
+config.read(config_path)
+updater = Updater(config.get('telegram','botapikey'), use_context=True)
 dispatcher = updater.dispatcher
 
 
 
 def start(update, context):
     print(update)
-    location_keyboard = KeyboardButton(text="создать метку", request_location=True)
+    location_keyboard = KeyboardButton(text="Создать метку", request_location=True)
 
     custom_keyboard = [[location_keyboard]]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-    context.bot.send_message(chat_id=update.message.chat_id, text="Would you mind sharing your location and contact with me?", reply_markup=reply_markup)
+    context.bot.send_message(chat_id=update.message.chat_id, text="Хотите создать метку на карте?", reply_markup=reply_markup)
 
-    #context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-
-def location(bot, update):
-    message = None
-    if update.edited_message:
-        message = update.edited_message
-    else:
-        message = update.message
-    current_pos = (message.location.latitude, message.location.longitude)
+def location
 
 start_handler = CommandHandler('start', start)
+dispatcher.add_handler(MessageHandler(Filters.text, location))
 dispatcher.add_handler(start_handler)
-
-
-
-
-
 
 updater.start_polling()
