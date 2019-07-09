454ab5cb74ac460a4ced383089a09458656b4fa1
jackv <jackv@MacBook-Pro-jackv.local>
2019-07-10 00:36:52 +0300

    edit ansible files

--- monitor_apps/src/test_map.py
+++
@@ -1,7 +0,0 @@
-import folium
-
-
-m = folium.Map(location=[52.720347, 58.665166], zoom_start=16)
-tooltip = 'info'
-folium.Marker([52.720347, 58.665166], popup='<i>хлороводород</i>', tooltip=tooltip).add_to(m)
-m.save('index.html')
