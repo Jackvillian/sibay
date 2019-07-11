92ec9dd3dbe153674cc4423123c2b8c879c5195b
jackv <jackv@MacBook-Pro-jackv.local>
2019-07-12 01:27:50 +0300

    change flow deploy

--- monitor_apps/scripts/deploy.sh
+++ monitor_apps/scripts/deploy.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
-Tag=$1
-echo "creating release"
-cd ../../
-branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-git checkout release
-git fetch
-git merge $branch
-git tag -a v$Tag -m 'app release'
-git push origin release
-echo "building"
-cd monitor_apps/scripts/
-sh build.sh $Tag
-cd ../../ansible
-ansible-playbook deploy.yml  -i inventory/hosts
-echo "deploy done"
+mode=$1
+Tag=$2
+case $mode in
+    docker)
+          echo "building"
+          cd monitor_apps/scripts/
+          sh build.sh $Tag
+          ;;
+    release)
+          echo "creating release"
+          cd ../../
+          branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+          git checkout release
+          git fetch
+          git merge $branch
+          git tag -a v$Tag -m 'app release'
+          git push origin release
+          ;;
+    deploy)
+          echo "deploying"
+          cd ../../ansible
+          cat /dev/null > vars/tags.yml
+          echo "---" >> vars/tags.yml
+          echo "tag: $Tag" >> vars/tags.yml
+          ansible-playbook deploy.yml  -i inventory/hosts
+          ;;
+esac
+
 echo $TAG >> Versions.txt
