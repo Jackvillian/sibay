8996fd6859a17be5e1a7a000679b5c6e50850bfc
jackv <jackv@MacBook-Pro-jackv.local>
2019-07-11 22:30:03 +0300

    test

--- monitor_apps/celery_app/entrypoint.sh
+++ monitor_apps/celery_app/entrypoint.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+cd /usr/src/app && alembic revision --autogenerate -m "Migrations"
+cd /usr/src/app && alembic upgrade head
+echo "Migrations ..."
 cd /usr/src/app && exec celery  -A worker_app worker -E
