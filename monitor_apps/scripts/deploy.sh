bd9d59c60e349cbb076772d69354e30f28d72ff8
jackv <jackv@MacBook-Pro-jackv.local>
2019-07-11 23:08:34 +0300

    fix gitignore

--- monitor_apps/scripts/deploy.sh
+++ monitor_apps/scripts/deploy.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 Tag=$1
+echo "creating release"
+cd ../../
 branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')
-git branch release$Tag
-git checkout release$Tag
-git merge branch
-#sh build.sh $Tag
-echo "building done"
-echo $branch
+git checkout release
+git fetch
+git merge $branch
+git tag -a v$Tag -m 'app release'
+git push origin release
+echo "building"
+cd monitor_apps/scripts/
+sh build.sh $Tag
+
+
 #cd ../ansible
 #ansible-playbook deploy.yml --extra-vars "tag=$Tag"
