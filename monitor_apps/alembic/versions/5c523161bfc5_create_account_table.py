c18a5be472cfa7f7fb262f9a44c346f10442e4c5
jackv <jackv@MacBook-Pro-jackv.local>
2019-07-25 20:08:34 +0300

    add migration

---
+++ monitor_apps/alembic/versions/5c523161bfc5_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 5c523161bfc5
+Revises: 5d66460b512a
+Create Date: 2019-07-25 20:07:44.929003
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5c523161bfc5'
+down_revision = '5d66460b512a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
